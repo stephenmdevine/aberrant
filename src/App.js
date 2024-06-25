712d343481dd1ce4314e56d3e44aa87432a9216d
stephenmdevine <stephenmdevine@gmail.com>
2024-06-25 15:07:17 -0500

    Fresh start

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
+import logo from './logo.svg';
 import './App.css';
-import { BrowserRouter as Route, Router, Routes } from 'react-router-dom';
-import Home from './pages/Home';
 
 function App() {
   return (
-    <div>
-      <Router>
-        <Routes>
-          <Route exact path='/' element={<Home />} />
-        </Routes>
-      </Router>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
